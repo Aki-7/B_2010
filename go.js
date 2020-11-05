d8f8316dd0fe4098869552528ae436a90e6337f8
Aki-7 <aki.develop8128@gmail.com>
2020-11-05 23:16:16 +0900

    update go.js

--- go.js
+++ go.js
@@ -3,11 +3,12 @@ dotenv.config();
 const { Octokit } = require("@octokit/core");
 
 function main() {
+  const branch = process.argv.length > 2 ? process.argv[2] : "main";
   const key = process.env["GITHUB_MY_TOKEN"];
   const octokit = new Octokit({ auth: key });
   octokit
     .request("POST /repos/jphacks/B_2010/deployments", {
-      ref: "main",
+      ref: branch,
     })
     .then(console.log)
     .catch(console.error);
