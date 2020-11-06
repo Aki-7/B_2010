e54520cd557748ac95d8ebfbf40bb1466cc37475
Aki-7 <aki.develop8128@gmail.com>
2020-11-06 11:41:30 +0900

    :sparkles: Enable to pay donation manually

--- src/controller/__tests__/donation_controller.ts
+++ src/controller/__tests__/donation_controller.ts
@@ -53,3 +53,39 @@ describe("POST /donation", () => {
     });
   });
 });
+
+describe("POST /donation/pay", () => {
+  describe("when not logged in", () => {
+    baseContext(server);
+    it("redirects to /login", async () => {
+      const res = await request(app)
+        .post("/donation/pay")
+        .send(
+          qs.stringify({
+            amount: 100,
+          })
+        );
+      expect(res.status).toBe(302);
+      expect(res.headers["location"]).toBe("/login");
+    });
+  });
+
+  describe("when authenticated", () => {
+    authContext(app, server);
+    it("redirect to /donation?message=Thank You", async () => {
+      const res = await request(app)
+        .post("/donation/pay")
+        .send(
+          qs.stringify({
+            amount: 100,
+          })
+        );
+      expect(res.status).toBe(302);
+      expect(res.headers["location"]).toBe(
+        `/donation?${qs.stringify({
+          message: "Thank You",
+        })}`
+      );
+    });
+  });
+});
